bfab0b64f9b536d590ef7a50408af6c7a04f0e36
Goran Lovincic <lovincic@gmail.com>
2023-03-25 23:38:15 +0100

    errors

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 https://jwt.io/introduction
 https://jwt.io/#debugger-io
 https://www.npmjs.com/package/jsonwebtoken
+https://www.npmjs.com/package/http-status-codes
--- controllers/main.js
+++ controllers/main.js
@@ -4,7 +4,7 @@
 // send back to front-end
 // setup authentication so only the request with JWT can access the dasboard
 const jwt = require('jsonwebtoken')
-const CustomAPIError = require('../errors/custom-error')
+const {BadRequestError} = require('../errors')
 const login = async (req, res) => {
     const { username, password } = req.body;
 
@@ -13,7 +13,7 @@ const login = async (req, res) => {
     // check in the controller
 
     if (!username || !password) {
-        throw new CustomAPIError('Please provide email and password', 400)
+        throw new BadRequestError ('Please provide email and password')
         // throw new BadRequestError('Please provide email and password')
     }
 
@@ -30,7 +30,7 @@ const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '3
 }
 
 const dashboard = async (req, res) => {
-console.log(req.user);
+// console.log(req.user);
 const luckyNumber = Math.floor(Math.random() * 10)
 res.status(200).json({
     msg: `Hello ${req.user.username}`,
